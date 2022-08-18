cfe5777ba0e963e804e807360c892162aa2bfbde
monginadiana <dianahmongina2@gmail.com>
2022-08-18 23:11:20 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Your code here
+// // Your code here
+// document.addEventListener("keydown", function (event) {
+//     console.log(event);
+//   });
+
+//  Helps you identify the type of code to write for an event listener
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+      const leftNumbers = dodger.style.left.replace("px", "");
+      const left = parseInt(leftNumbers, 10);
+  
+      dodger.style.left = `${left - 1}px`;
+    }
+  });
+
+  function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+  const moveDodgerRight = () => {
+    const left = '180px';
+    dodger.style.left = left;
+}
